0ed3d8d7850ef0c13043112a40fb3770e4008ffe
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-10-09 17:52:03 -0700

    Fix entity flags, add surface flags

--- lua/gangbox/modules/flags/flags.lua
+++ lua/gangbox/modules/flags/flags.lua
@@ -1,61 +1,156 @@
 AddCSLuaFile( "values.lua" )
 include( "values.lua" )
 
+local Bitflags = gb.Bitflags
+
 local headerColor = Color( 220, 220, 220 )
 local nameColor = Color( 200, 200, 255 )
 local valueColor = Color( 255, 255, 0 )
 local descColor = Color( 200, 255, 200 )
 local infoColor = Color( 255, 200, 200 )
+local darkInfoColor = Color( 255, 150, 150 )
+
+local assert = assert
+local tostring = tostring
+local bit_band = bit.band
+local string_Comma = string.Comma
 
 --- Parses a bitflag and returns its components as a table, printing them with MsgC in a beautiful format.
 --- @param bitflag number The bitflag to parse.
 --- @param flagset gb.FlagSet The flagset to use for parsing.
 local function parseFlags( bitflag, flagset )
+    local prefix = "  "
+
     for name, flagValue in SortedPairsByMemberValue( flagset, "value" ) do
-        if bit.band( bitflag, flagValue.value ) ~= 0 then
-            MsgC( nameColor, name .. " - " )
+        if bit_band( bitflag, flagValue.value ) ~= 0 then
+            MsgC( nameColor, prefix, name .. " - " )
             MsgC( valueColor, tostring( flagValue.value ), "\n" )
             MsgC( descColor, "    ", flagValue.description, "\n\n" )
         end
     end
 end
 
+--- Checks a given table to see if it's a TraceResult structure
+--- @param tbl table The table to check
+--- @return boolean isTrace
+local function isTrace( tbl )
+    local traceKeys = { "AllSolid", "DispFlags", "HitTexture", "StartSolid", "SurfaceFlags" }
+
+    for _, key in ipairs( traceKeys ) do
+        if tbl[key] == nil then return false end
+    end
+
+    return true
+end
+
+
 function gb.ParseFlags( subject, flagset )
     local typeID = TypeID( subject )
 
     if typeID == TYPE_NUMBER then
-        assert( flagset, "No flagset provided for bitflag (check the gb.Bitflags table)" )
+        assert( flagset, "No flagset provided for bitflag (check the gb.Bitflags table, or pass the subject directly, rather than their flags)" )
         parseFlags( subject, flagset )
 
     elseif typeID == TYPE_CONVAR then
         MsgC( headerColor, "Flag values for " )
         MsgC( infoColor, tostring( subject ), "\n" )
-        parseFlags( subject:GetFlags(), gb.Bitflags.Convar )
+        parseFlags( subject:GetFlags(), Bitflags.Convar )
 
     elseif typeID == TYPE_ENTITY then
-        MsgC( headerColor, "Entity Flags for: " )
+        MsgC( headerColor, "Flags for: " )
         MsgC( infoColor, tostring( subject ), "\n\n" )
-        parseFlags( subject:GetFlags(), gb.Bitflags.Entity )
+
+        do
+            local flags = subject.GetFlags and subject:GetFlags() or 0
+            if flags ~= 0 then
+                MsgC( headerColor, "Entity Flags:\n" )
+                parseFlags( flags, Bitflags.Entity )
+            end
+        end
+
+        do
+            local eFlags = subject.GetEFlags and subject:GetEFlags() or 0
+            if eFlags ~= 0 then
+                MsgC( headerColor, "Engine Flags:\n" )
+                parseFlags( eFlags, Bitflags.EntityEngine )
+            end
+        end
+
+        do
+            local spawnFlags = subject.GetSpawnFlags and subject:GetSpawnFlags() or 0
+            if spawnFlags ~= 0 then
+                MsgC( headerColor, "Spawn Flags:\n" )
+                parseFlags( spawnFlags, Bitflags.EntitySpawn )
+            end
+        end
+
+        do
+            local solidFlags = subject.GetSolidFlags and subject:GetSolidFlags() or 0
+            if solidFlags ~= 0 then
+                MsgC( headerColor, "Solid Flags:\n" )
+                parseFlags( solidFlags, Bitflags.EntitySolid )
+            end
+        end
 
     elseif typeID == TYPE_DAMAGEINFO then
-        MsgC( headerColor, "Damage Types for: " )
-        MsgC( infoColor, string.Comma( subject:GetDamageAmount() ) )
-        MsgC( headerColor, " | " )
-        MsgC( infoColor, tostring( subject:GetAttacker() ) )
-        MsgC( headerColor, ">" )
-        MsgC( infoColor, tostring( subject:GetInflictor() ), "\n" )
+        MsgC( headerColor, "Damage Type Flags for: " )
 
-        parseFlags( subject:GetDamageType(), gb.Bitflags.Damage )
+        MsgC( valueColor, tostring( subject:GetAttacker() ) )
+        MsgC( headerColor, " > " )
+        MsgC( darkInfoColor, string_Comma( subject:GetDamage() ), " " )
+
+        local bonus = subject:GetDamageBonus()
+        if bonus ~= 0 then
+            MsgC( headerColor, "(" )
+            MsgC( descColor, "+", bonus )
+            MsgC( headerColor, ") " )
+        end
+        MsgC( headerColor, "\n" )
+
+        parseFlags( subject:GetDamageType(), Bitflags.Damage )
 
     elseif typeID == TYPE_MATERIAL then
         MsgC( headerColor, "Material flags for: " )
         MsgC( infoColor, subject:GetName(), "\n\n" )
 
-        MsgC( infoColor, "[\"$flags\"]:", "\n" )
-        parseFlags( subject:GetInt( "$flags" ), gb.Bitflags.MaterialFlags )
+        do
+            local flags = subject:GetInt( "$flags" ) or 0
+            if flags ~= 0 then
+                MsgC( headerColor, "[\"$flags\"]:", "\n" )
+                parseFlags( flags, Bitflags.MaterialFlags )
+            end
+        end
 
-        MsgC( infoColor, "[\"$flags2\"]:", "\n" )
-        parseFlags( subject:GetInt( "$flags2" ), gb.Bitflags.MaterialFlags2 )
+        do
+            local flags2 = subject:GetInt( "$flags2" ) or 0
+            if flags2 ~= 0 then
+                MsgC( headerColor, "[\"$flags2\"]:", "\n" )
+                parseFlags( flags2, Bitflags.MaterialFlags2 )
+            end
+        end
+
+    elseif typeID == TYPE_TABLE then
+        if isTrace( subject ) then
+            MsgC( headerColor, "Surface Flags for:  " )
+            MsgC( infoColor, subject.HitTexture, "\n\n" )
+
+            do
+                local surfaceFlags = subject.SurfaceFlags or 0
+                if surfaceFlags ~= 0 then
+                    MsgC( headerColor, "SurfaceFlags:\n" )
+                    parseFlags( surfaceFlags, Bitflags.Surface )
+                end
+            end
+
+            do
+                local dispFlags = subject.DispFlags or 0
+                if dispFlags ~= 0 then
+                    MsgC( headerColor, "DispFlags:\n" )
+                    parseFlags( dispFlags, Bitflags.DispSurface )
+                end
+            end
+        end
+
     else
         error( "Cannot parse flags for type " .. type( subject ) .. " (" .. tostring( typeID ) .. ")" )
     end
--- lua/gangbox/modules/flags/values.lua
+++ lua/gangbox/modules/flags/values.lua
@@ -10,6 +10,9 @@
 --- @field Damage gb.FlagSet
 --- @field Convar gb.FlagSet
 --- @field Entity gb.FlagSet
+--- @field EntitySpawn gb.FlagSet
+--- @field EntitySolid gb.FlagSet
+--- @field EntityEngine gb.FlagSet
 --- @field Texture gb.FlagSet
 gb.Bitflags = {
 
@@ -280,16 +283,16 @@ gb.Bitflags = {
         },
     },
 
-    --- FL_ Entity Flags
+    --- FL_ Entity Flags (:GetFlags())
     Entity = {
         FL_ONGROUND = {
-            value = 1, description = "Is the entity on ground or not"
+            value = 1, description = "Entity is on the ground"
         },
         FL_DUCKING = {
-            value = 2, description = "Is player ducking or not"
+            value = 2, description = "Entity is Ducking"
         },
         FL_ANIMDUCKING = {
-            value = 4, description = "Is the player in the process of ducking or standing up"
+            value = 4, description = "The player is in the process of ducking or standing up"
         },
         FL_WATERJUMP = {
             value = 8, description = "The player is jumping out of water"
@@ -307,13 +310,13 @@ gb.Bitflags = {
             value = 128, description = "This player is controlling something UI related in the world, this prevents his movement, but doesn't freeze mouse movement, jumping, etc."
         },
         FL_CLIENT = {
-            value = 256, description = "Is this entity a player or not"
+            value = 256, description = "Entity is a Player"
         },
         FL_FAKECLIENT = {
             value = 512, description = "Bots have this flag"
         },
         FL_INWATER = {
-            value = 1024, description = "Is the player in water or not"
+            value = 1024, description = "Entity is in the water"
         },
         FL_FLY = {
             value = 2048, description = "This entity can fly"
@@ -328,10 +331,10 @@ gb.Bitflags = {
             value = 16384, description = "NPCs have this flag (NPC: Ignore player push)"
         },
         FL_GODMODE = {
-            value = 32768, description = "Whether the player has god mode enabled"
+            value = 32768, description = "Player has God Mode enabled"
         },
         FL_NOTARGET = {
-            value = 65536, description = "Makes the entity invisible to AI"
+            value = 65536, description = "Entity is invisible to AI"
         },
         FL_AIMTARGET = {
             value = 131072, description = "This entity can be aimed at"
@@ -340,13 +343,13 @@ gb.Bitflags = {
             value = 262144, description = "Not all corners are valid"
         },
         FL_STATICPROP = {
-            value = 524288, description = "It's a static prop"
+            value = 524288, description = "Entity is a Static Prop"
         },
         FL_GRAPHED = {
             value = 1048576, description = "worldgraph has this ent listed as something that blocks a connection"
         },
         FL_GRENADE = {
-            value = 2097152, description = "This entity is a grenade, unused"
+            value = 2097152, description = "This entity is a grenade (unused)"
         },
         FL_STEPMOVEMENT = {
             value = 4194304, description = "Changes the SV_Movestep() behavior to not do any processing"
@@ -380,6 +383,237 @@ gb.Bitflags = {
         },
     },
 
+    --- Entity Spawn SF_ bitflags (:GetSpawnFlags())
+    --- TODO: How to handle entity-specific spawn flags?
+    --- I commented out the ones that seem less likely to be used
+    EntitySpawn = {
+        SF_NPC_WAIT_TILL_SEEN = {
+            value = 1, description = "Wait till seen"
+        },
+        SF_NPC_GAG = {
+            value = 2, description = "No IDLE sounds until angry"
+        },
+        SF_NPC_FALL_TO_GROUND = {
+            value = 4, description = "If not set, means teleport to ground"
+        },
+        -- SF_PHYSPROP_MOTIONDISABLED = {
+        --     value = 8, description = "If set, calls PhysObj:EnableMotion( false ) on the func_physbox when the physics are created. See Physics optimization."
+        -- },
+        SF_NPC_DROP_HEALTHKIT = {
+            value = 8, description = "NPC Drops health kit when it dies"
+        },
+        SF_NPC_START_EFFICIENT = {
+            value = 16, description = "Don't acquire enemies or avoid obstacles"
+        },
+        SF_NPC_WAIT_FOR_SCRIPT = {
+            value = 128, description = "Wait for script"
+        },
+        SF_NPC_LONG_RANGE = {
+            value = 256, description = "Long Visibility/Shoot"
+        },
+        SF_NPC_FADE_CORPSE = {
+            value = 512, description = "Fade Corpse"
+        },
+        -- SF_FLOOR_TURRET_CITIZEN = {
+        --     value = 512, description = "Makes the floor turret friendly"
+        -- },
+        SF_NPC_ALWAYSTHINK = {
+            value = 1024, description = "Think outside PVS"
+        },
+        SF_NPC_TEMPLATE = {
+            value = 2048, description = "This entity is a template for the npc_template_maker. It will not spawn automatically and cannot be used with point_template."
+        },
+        SF_NPC_ALTCOLLISION = {
+            value = 4096, description = "Do Alternate collision for this NPC (player avoidance)"
+        },
+        SF_NPC_NO_WEAPON_DROP = {
+            value = 8192, description = "NPC Doesn't drop weapon on death"
+        },
+        SF_NPC_NO_PLAYER_PUSHAWAY = {
+            value = 16384, description = "Ignore player push - Don't give way to player"
+        },
+        SF_PHYSBOX_MOTIONDISABLED = {
+            value = 32768, description = "If set, calls PhysObj:EnableMotion( false ) on the func_physbox when the physics are created"
+        },
+        SF_CITIZEN_FOLLOW = {
+            value = 65536, description = "Follow the player as soon as I spawn"
+        },
+        -- SF_ROLLERMINE_FRIENDLY = {
+        --     value = 65536, description = "Makes the rollermine friendly."
+        -- },
+        SF_CITIZEN_MEDIC = {
+            value = 131072, description = "Makes the citizen a medic"
+        },
+        SF_CITIZEN_RANDOM_HEAD = {
+            value = 262144, description = "Gives the citizen a random head"
+        },
+        SF_CITIZEN_AMMORESUPPLIER = {
+            value = 524288, description = "Citizen that resupplies ammo"
+        },
+        SF_PHYSBOX_ALWAYS_PICK_UP = {
+            value = 1048576, description = "Gravity gun is ALWAYS allowed to pick this up."
+        },
+        -- SF_CITIZEN_NOT_COMMANDABLE = {
+        --     value = 1048576, description = "Citizen cannot join players squad, and will not able to be commanded by the Half-Life 2 command system for Citizens"
+        -- },
+        -- SF_CITIZEN_IGNORE_SEMAPHORE = {
+        --     value = 2097152, description = "Work outside the speech semaphore system"
+        -- },
+        SF_PHYSBOX_NEVER_PICK_UP = {
+            value = 2097152, description = "Gravity gun is NOT allowed to pick this up."
+        },
+        -- SF_CITIZEN_RANDOM_HEAD_MALE = {
+        --     value = 4194304, description = "Gives the citizen a random male head"
+        -- },
+        SF_PHYSBOX_NEVER_PUNT = {
+            value = 4194304, description = "Gravity gun is NOT allowed to punt this entity."
+        },
+        SF_CITIZEN_RANDOM_HEAD_FEMALE = {
+            value = 8388608, description = "Gives the citizen a random female head"
+        },
+        SF_CITIZEN_USE_RENDER_BOUNDS = {
+            value = 16777216, description = "Use render bounds instead of human hull for guys sitting in chairs, etc. Must be set before Spawn() is called to take effect"
+        },
+    },
+
+    --- Entity Solid flags (:GetSolidFlags())
+    EntitySolid = {
+        FSOLID_CUSTOMRAYTEST = {
+            value = 1, description = "Ignore solid type + always call into the entity for ray tests"
+        },
+        FSOLID_CUSTOMBOXTEST = {
+            value = 2, description = "Ignore solid type + always call into the entity for swept box tests"
+        },
+        FSOLID_NOT_SOLID = {
+            value = 4, description = "The object is currently not solid"
+        },
+        FSOLID_TRIGGER = {
+            value = 8, description = "This is something may be collideable but fires touch functions even when it's not collideable (when the FSOLID_NOT_SOLID flag is set)"
+        },
+        FSOLID_MAX_BITS = {
+            value = 10, description = "The amount of bits needed to store the all the flags in a variable/sent over network."
+        },
+        FSOLID_NOT_STANDABLE = {
+            value = 16, description = "Players can't stand on this"
+        },
+        FSOLID_VOLUME_CONTENTS = {
+            value = 32, description = "Contains volumetric contents (like water)"
+        },
+        FSOLID_FORCE_WORLD_ALIGNED = {
+            value = 64, description = "Forces the collision representation to be world-aligned even if it's SOLID_BSP or SOLID_VPHYSICS"
+        },
+        FSOLID_USE_TRIGGER_BOUNDS = {
+            value = 128, description = "Uses a special trigger bounds separate from the normal OBB"
+        },
+        FSOLID_ROOT_PARENT_ALIGNED = {
+            value = 256, description = "Collisions are defined in root parent's local coordinate space"
+        },
+        FSOLID_TRIGGER_TOUCH_DEBRIS = {
+            value = 512, description = "This trigger will touch debris objects"
+        },
+    },
+
+    --- Entity EFL_ bitflags (:GetEFlags())
+    EntityEngine = {
+        EFL_KILLME = {
+            value = 1, description = "This entity is marked for death -- This allows the game to actually delete ents at a safe time."
+        },
+        EFL_DORMANT = {
+            value = 2, description = "Entity is dormant, no updates to client"
+        },
+        EFL_NOCLIP_ACTIVE = {
+            value = 4, description = "Lets us know when the noclip command is active"
+        },
+        EFL_SETTING_UP_BONES = {
+            value = 8, description = "Set while a model is setting up its bones"
+        },
+        -- EFL_KEEP_ON_RECREATE_ENTITIES = {
+        --     value = 16, description = "This is a special entity that should not be deleted when we restart entities only"
+        -- },
+        EFL_HAS_PLAYER_CHILD = {
+            value = 16, description = "One of the child entities is a player"
+        },
+        EFL_DIRTY_SHADOWUPDATE = {
+            value = 32, description = "(Client only) need shadow manager to update the shadow"
+        },
+        EFL_NOTIFY = {
+            value = 64, description = "Another entity is watching events on this entity (used by teleport)"
+        },
+        EFL_FORCE_CHECK_TRANSMIT = {
+            value = 128, description = "The default behavior in ShouldTransmit is to not send an entity if it doesn't have a model. Certain entities want to be sent anyway because all the drawing logic is in the client DLL. They can set this flag and the engine will transmit them even if they don't have model"
+        },
+        EFL_BOT_FROZEN = {
+            value = 256, description = "This is set on bots that are frozen"
+        },
+        EFL_SERVER_ONLY = {
+            value = 512, description = "Non-networked entity"
+        },
+        EFL_NO_AUTO_EDICT_ATTACH = {
+            value = 1024, description = "Don't attach the edict"
+        },
+        EFL_DIRTY_ABSTRANSFORM = {
+            value = 2048, description = "?"
+        },
+        EFL_DIRTY_ABSVELOCITY = {
+            value = 4096, description = "?"
+        },
+        EFL_DIRTY_ABSANGVELOCITY = {
+            value = 8192, description = "Some dirty bits with respect to abs computations"
+        },
+        EFL_DIRTY_SURROUNDING_COLLISION_BOUNDS = {
+            value = 16384, description = "?"
+        },
+        EFL_DIRTY_SPATIAL_PARTITION = {
+            value = 32768, description = "?"
+        },
+        EFL_IN_SKYBOX = {
+            value = 131072, description = "This is set if the entity detects that it's in the skybox. This forces it to pass the 'in PVS' for transmission"
+        },
+        EFL_USE_PARTITION_WHEN_NOT_SOLID = {
+            value = 262144, description = "Entity shows up in the partition even when not solid"
+        },
+        EFL_TOUCHING_FLUID = {
+            value = 524288, description = "Used to determine if an entity is floating"
+        },
+        EFL_IS_BEING_LIFTED_BY_BARNACLE = {
+            value = 1048576, description = "?"
+        },
+        EFL_NO_ROTORWASH_PUSH = {
+            value = 2097152, description = "?"
+        },
+        EFL_NO_THINK_FUNCTION = {
+            value = 4194304, description = "Avoid executing the entity's Think"
+        },
+        EFL_NO_GAME_PHYSICS_SIMULATION = {
+            value = 8388608, description = "?"
+        },
+        EFL_CHECK_UNTOUCH = {
+            value = 16777216, description = "?"
+        },
+        EFL_DONTBLOCKLOS = {
+            value = 33554432, description = "Entity shouldn't block NPC line-of-sight"
+        },
+        EFL_DONTWALKON = {
+            value = 67108864, description = "NPCs should not walk on this entity"
+        },
+        EFL_NO_DISSOLVE = {
+            value = 134217728, description = "The entity shouldn't dissolve"
+        },
+        EFL_NO_MEGAPHYSCANNON_RAGDOLL = {
+            value = 268435456, description = "Mega physcannon can't ragdoll these guys"
+        },
+        EFL_NO_WATER_VELOCITY_CHANGE = {
+            value = 536870912, description = "Don't adjust this entity's velocity when transitioning into water"
+        },
+        EFL_NO_PHYSCANNON_INTERACTION = {
+            value = 1073741824, description = "Physcannon can't pick these up or punt them"
+        },
+        EFL_NO_DAMAGE_FORCES = {
+            value = -2147483648, description = "Doesn't accept forces from physics damage"
+        }
+    },
+
+    --- Convar FCVAR_ bitflags
     Convar = {
         FCVAR_UNREGISTERED = {
             value = 1, description = "If this is set, the convar will become anonymous and won't show up in the 'find' results"
@@ -549,5 +783,76 @@ gb.Bitflags = {
         TEXTUREFLAGS_UNUSED_80000000 = {
             value = 2147483648, description = "Unused flag, reserved for future use"
         },
-    }
+    },
+
+    --- Surface SURF_ bitflags, used in TraceResult structures
+    Surface = {
+        SURF_LIGHT = {
+            value = 1, description = "Value will hold the light strength"
+        },
+        SURF_SKY2D = {
+            value = 2, description = "This surface is a 2D skybox"
+        },
+        SURF_SKY = {
+            value = 4, description = "This surface is a skybox, equivalent to HitSky in Structures/TraceResult"
+        },
+        SURF_WARP = {
+            value = 8, description = "This surface is animated water"
+        },
+        SURF_TRANS = {
+            value = 16, description = "This surface is translucent"
+        },
+        SURF_NOPORTAL = {
+            value = 32, description = "This surface cannot have portals placed on, used by Portal's gun"
+        },
+        SURF_TRIGGER = {
+            value = 64, description = "This surface is a trigger"
+        },
+        SURF_NODRAW = {
+            value = 128, description = "This surface is an invisible entity, equivalent to HitNoDraw in Structures/TraceResult"
+        },
+        SURF_HINT = {
+            value = 256, description = "Make a primary bsp splitter"
+        },
+        SURF_SKIP = {
+            value = 512, description = "This surface can be ignored by impact effects"
+        },
+        SURF_NOLIGHT = {
+            value = 1024, description = "This surface has no lights calculated"
+        },
+        SURF_BUMPLIGHT = {
+            value = 2048, description = "Three lightmaps are calculated for this surface for bumpmapping"
+        },
+        SURF_NOSHADOWS = {
+            value = 4096, description = "No shadows are cast on this surface"
+        },
+        SURF_NODECALS = {
+            value = 8192, description = "No decals are applied to this surface"
+        },
+        SURF_NOCHOP = {
+            value = 16384, description = "Patches are not subdivided on this surface"
+        },
+        SURF_HITBOX = {
+            value = 32768, description = "This surface is part of an entity's hitbox"
+        }
+    },
+
+    --- Displacement Surface DISPSURF_ bitflags, used in TraceResult structures
+    DispSurface = {
+        DISPSURF_SURFACE = {
+            value = 1, description = "Surface is Displacement"
+        },
+        DISPSURF_WALKABLE = {
+            value = 2, description = "Displacement is walkable"
+        },
+        DISPSURF_BUILDABLE = {
+            value = 4, description = "?"
+        },
+        DISPSURF_SURFPROP1 = {
+            value = 8, description = "?"
+        },
+        DISPSURF_SURFPROP2 = {
+            value = 16, description = "?"
+        }
+    },
 }
