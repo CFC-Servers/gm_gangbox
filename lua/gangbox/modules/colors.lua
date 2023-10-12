955971662443510e8218f34b4f85a5d07847b6f9
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-10-11 17:29:52 -0700

    Grep sources

--- lua/gangbox/modules/colors.lua
+++ lua/gangbox/modules/colors.lua
@@ -2,6 +2,7 @@ gb.Colors = {
     bold = Color( 48, 188, 237, 255 ),
     details = Color( 5, 60, 94, 255 ),
     text = Color( 255, 250, 255, 255 ),
+    subtext = Color( 175, 175, 175, 255 ),
     bad = Color( 252, 81, 48, 255 ),
     highlight = Color( 233, 255, 112, 255 ),
 }
--- lua/gangbox/modules/grep.lua
+++ lua/gangbox/modules/grep.lua
@@ -1,69 +1,220 @@
 local colors = gb.Colors
 
-local finished = colors.bold
-local softWhite = colors.text
-local foundRed = colors.bad
--- local pink = Color( 235, 52, 137 )
-local pink = colors.highlight
+local bold = colors.bold
+local text = colors.text
+local subtext = colors.subtext
+local bad = colors.bad
+local highlight = colors.highlight
 
-local next = next
 local ipairs = ipairs
 local file_Find = file.Find
 local table_insert = table.insert
 local coroutine_yield = coroutine.yield
 local coroutine_resume = coroutine.resume
+local coroutine_status = coroutine.status
 
+--- @class Provider
+--- @field source string The Provider's source (e.g. Workshop/Legacy/Content)
+--- @field name string The name of the Provider (e.g. the workshop name, game name)
+--- @field details string? Any additional details about the Provider
 
-local function findFiles( path, scope, foundFiles )
-    if not path then path = "" end
-    if not foundFiles then foundFiles = {} end
+--- Searches Workshop Addons to find which one provides the given path
+--- @param path string The full path of the file
+local function findWorkshopSource( path )
+    local addons = engine.GetAddons()
+    local addonCount = #addons
 
-    local fils, dirs = file_Find( path .. "*", scope )
+    for i = 1, addonCount do
+        local addon = addons[i]
 
-    if fils and next( fils ) ~= 0 then
-        for _, fil in ipairs( fils ) do
-            table_insert( foundFiles, path .. fil )
+        local results = file_Find( path, addon.title )
+        if #results > 0 then
+            return addon
         end
-    end
 
-    if dirs and next( dirs ) ~= 0 then
-        for _, dir in ipairs( dirs ) do
-            local searchPath = path .. dir .. "/"
-            coroutine_yield()
-            findFiles( searchPath, scope, foundFiles )
-        end
+        coroutine_yield()
     end
-
-    return foundFiles
 end
 
+local string_Split = string.Split
+local string_StartsWith = string.StartsWith
+--- Finds files added from Addons (Only works serverside)
+--- @param path string The full path of the file
+local function findAddonSource( path )
+    if string_StartsWith( path, "addons/" ) then
+        local spl = string_Split( path, "/" )
+        local addonName = spl[2]
 
-local searchString
-do
-    local string_sub = string.sub
-    local string_find = string.find
+        return addonName
+    end
 
-    searchString = function( stringToFind, scope, path, printer )
-        local allFiles = findFiles( path, scope )
+    local _, addons = file_Find( "addons/*", "MOD" )
+    local addonCount = #addons
 
-        for _, filePath in ipairs( allFiles ) do
-            local start, fin = string_find( filePath, stringToFind )
-            if start then
-                local prefix = string_sub( filePath, 1, start - 1 )
-                local main = string_sub( filePath, start, fin )
-                local suffix = string_sub( filePath, fin + 1 )
+    for i = 1, addonCount do
+        local addon = addons[i]
 
-                printer( softWhite, prefix )
-                printer( foundRed, main )
-                printer( softWhite, suffix, "\n" )
+        local results = file_Find( "addons/" .. path, "MOD" )
+        if #results > 0 then
+            return addon
+        end
+
+        coroutine_yield()
+    end
+end
+
+--- Finds files added from Mounted Games
+--- @param path string The full path of the file
+local function findGameSource( path )
+    local games = {}
+
+    do
+        local allGames = engine.GetGames()
+        local allGamesCount = #allGames
+
+        for i = 1, allGamesCount do
+            local game = allGames[i]
+            if game.mounted then
+                table_insert( games, game )
             end
         end
     end
+
+    local gamesCount = #games
+
+    for i = 1, gamesCount do
+        local game = games[i]
+
+        local results = file_Find( path, game.folder )
+        if #results > 0 then
+            return game
+        end
+
+        coroutine_yield()
+    end
+end
+
+--- Finds the provider of the given file path
+--- @param path string The path to the file to inspect
+--- @return Provider? information about the Provider 
+local function findProvider( path )
+    local provider
+
+    provider = findWorkshopSource( path )
+    if provider then
+        return { source = "Workshop", name = provider.title, details = provider.wsid }
+    end
+
+    provider = findAddonSource( path )
+    if provider then
+        return { source = "Addon", name = provider }
+    end
+
+    provider = findGameSource( path )
+    if provider then
+        return { source = "Game", name = provider.title }
+    end
+end
+
+--- Gathers and returns a table of all filnames that match the given search string
+--- (Recursive, yields the results)
+local function findFiles( path, scope )
+    if not path then path = "" end
+
+    local fils, dirs = file_Find( path .. "*", scope )
+    if fils then
+        for _, fil in ipairs( fils ) do
+            coroutine_yield( path .. fil )
+        end
+    end
+
+    if dirs then
+        for _, dir in ipairs( dirs ) do
+            local searchPath = path .. dir .. "/"
+            findFiles( searchPath, scope )
+        end
+    end
+end
+
+local string_sub = string.sub
+local string_find = string.find
+
+local function printResults( printer, results )
+    local resultCount = #results
+
+    for i = 1, resultCount do
+        local result = results[i]
+
+        local filePath = result.filePath
+        local provider = result.provider
+        local start = result.start
+        local fin = result.fin
+
+        local prefix = string_sub( filePath, 1, start - 1 )
+        local main = string_sub( filePath, start, fin )
+        local suffix = string_sub( filePath, fin + 1 )
+
+        printer( text, prefix )
+        printer( bad, main )
+        printer( text, suffix, "\n" )
+
+        if provider then
+            printer( highlight, "  └ " )
+            printer( bold, provider.source )
+            printer( text, " - " )
+            printer( text, provider.name )
+
+            if provider.details then
+                printer( text, " (" )
+                printer( subtext, provider.details )
+                printer( text, ")" )
+            end
+
+            printer( "\n" )
+        end
+    end
+end
+
+-- TODO: Refactor this to only one coroutine
+local function searchString( stringToFind, scope, path )
+    -- Here we make a sub-coroutine to actually find the files
+    local findResults = {}
+    local finder = coroutine.create( findFiles )
+
+    while true do
+        coroutine_yield()
+
+        if coroutine_status( finder ) == "dead" then
+            break
+        end
+
+        local success, output = coroutine_resume( finder, path, scope )
+        if not success then error( output ) end
+
+        if output then
+            local filePath = output
+
+            local start, fin = string_find( filePath, stringToFind, 1, true )
+            if start then
+                local provider = findProvider( filePath )
+                table_insert( findResults, {
+                    filePath = filePath,
+                    provider = provider,
+                    start = start,
+                    fin = fin
+                } )
+            end
+        end
+    end
+
+    return findResults
 end
 
 local function startSearch( str, scope, path, printer )
     local co = coroutine.create( function()
-        searchString( str, scope, path, printer )
+        local findResults = searchString( str, scope, path )
+        printResults( printer, findResults )
+
         coroutine_yield( "done" )
     end )
 
@@ -72,11 +223,16 @@ local function startSearch( str, scope, path, printer )
         local max = SysTime() + ( engine.TickInterval() * 0.75 )
 
         while SysTime() < max do
-            local ran, done = coroutine_resume( co )
-            if not ran or done == "done" then
+            if coroutine_status( co ) == "dead" then
                 hook.Remove( "Think", "grepCoroutineHook" )
-                printer( finished, "Done!", "\n" )
+                printer( bold, "Done!", "\n" )
                 break
+            end
+
+            local success, output = coroutine_resume( co )
+            if not success then
+                hook.Remove( "Think", "grepCoroutineHook" )
+                error( output )
             end
         end
     end )
@@ -84,12 +240,12 @@ end
 
 local function startGrep( str, scope, path, printer )
     if not str or str == "" or #str < 3 then
-        printer( pink, "Please enter a string, strings have to be a minimum of 3 characters long!", "\n" )
+        printer( highlight, "Please enter a string, strings have to be a minimum of 3 characters long!", "\n" )
         return
     end
 
     startSearch( str, scope, path, printer )
-    printer( pink, "Started grep search...", "\n" )
+    printer( highlight, "Started grep search...", "\n" )
 end
 
 --- Performs an asynchronous text search in all GMod file names and prints them
@@ -112,7 +268,7 @@ do
         local printer = SERVER and AnsiC or MsgC
 
         if IsValid( ply ) and not ply:IsSuperAdmin() then
-            printer( pink, "No permission.", "\n" )
+            printer( highlight, "No permission.", "\n" )
             return
         end
 
